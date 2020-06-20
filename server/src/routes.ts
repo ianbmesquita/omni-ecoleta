ec7b55ad8f41f77996a8804e49f190e088723f71
Ian Bezerra de Mesquita <ian.mesquita@inep.gov.br>
2020-06-20 13:15:02 -0300

    Criação da rota de listagem de itens

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,24 @@
 import express from 'express'
+import knex from './database/connection'
 
 const routes = express.Router()
 
 routes.get('/', (request, response) => {
     return response.json('Servidor conectado na porta 3333...')
+})
+
+routes.get('/items', async (request, response) => {
+    const items = await knex('items').select('*')
+
+    const serializedItems = items.map(item => {
+        return {
+            id: item.id,
+            title: item.name,
+            image_url: `http://localhost:3333/image/${item.image}`
+        }
+    })
+
+    return response.json(serializedItems)
 })
 
 export default routes
