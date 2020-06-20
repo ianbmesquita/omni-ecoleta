2dbf64ab603cbd5179a72565529a1490ae899176
Ian Bezerra de Mesquita <ian.mesquita@inep.gov.br>
2020-06-20 12:56:54 -0300

    Criação dos seeds para geração do DML de INSERT na tabela items + adição das imagens

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,12 @@
 import express from 'express'
+import path from 'path'
 import routes from './routes'
 
 const app = express()
 
 app.use(express.json())
 app.use(routes)
+
+app.use('/image', express.static(path.resolve(__dirname, '..', 'assets', 'images')))
 
 app.listen(3333)
