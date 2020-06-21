367530538f6074232d6378d3273ab24679fefd2c
Ian Bezerra de Mesquita <ian.mesquita@inep.gov.br>
2020-06-21 13:17:04 -0300

    Criação da API de busca de todos os items de coleta

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,13 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { FiArrowLeft } from 'react-icons/fi'
 import { Map, TileLayer, Marker } from 'react-leaflet'
 
-
 import './styles.css'
 import logo from '../../assets/images/logo.svg'
+import api from '../../services/api'
+
+interface Item {
+    id: number
+    title: string
+    image_url: string
+}
 
 const CreatePoint = () => {
+    const [items, setItems] = useState<Item[]>([])
+    
+    useEffect(() => {
+        api.get('items').then(response => {
+            setItems(response.data)
+        })
+    }, [])
+    
     return (
         <div id="page-create-point">
             <header>
@@ -93,35 +107,12 @@ const CreatePoint = () => {
                     </legend>
 
                     <ul className="items-grid">
-                        <li className="selected">
-                            <img src="http://localhost:3333/image/lampadas.svg" alt="Lâmpadas"/>
-                            <span>Lâmpadas</span>
-                        </li>
-
-                        <li>
-                            <img src="http://localhost:3333/image/lampadas.svg" alt="Lâmpadas"/>
-                            <span>Lâmpadas</span>
-                        </li>
-
-                        <li>
-                            <img src="http://localhost:3333/image/lampadas.svg" alt="Lâmpadas"/>
-                            <span>Lâmpadas</span>
-                        </li>
-
-                        <li>
-                            <img src="http://localhost:3333/image/lampadas.svg" alt="Lâmpadas"/>
-                            <span>Lâmpadas</span>
-                        </li>
-
-                        <li>
-                            <img src="http://localhost:3333/image/lampadas.svg" alt="Lâmpadas"/>
-                            <span>Lâmpadas</span>
-                        </li>
-
-                        <li className="selected">
-                            <img src="http://localhost:3333/image/lampadas.svg" alt="Lâmpadas"/>
-                            <span>Lâmpadas</span>
-                        </li>
+                        {items.map(item => (
+                            <li key={item.id}>
+                                <img src={item.image_url} alt={item.title}/>
+                                <span>{item.title}</span>
+                            </li>
+                        ))}
                     </ul>
                 </fieldset>
 
