8d2469c05db5cea31f41630d1bc3a2fab724e242
Ian Bezerra de Mesquita <ian.mesquita@inep.gov.br>
2020-06-22 09:23:31 -0300

    Criação do componente de upload de arquivos

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -9,6 +9,8 @@ import './styles.css'
 import logo from '../../assets/images/logo.svg'
 import api from '../../services/api'
 
+import Dropzone from '../../components/dropzone'
+
 interface Item {
     id: number
     title: string
@@ -38,6 +40,7 @@ const CreatePoint = () => {
     const [selectedCity, setSelectedCity] = useState('0')
     const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0, 0])
     const [selectedItems, setSelectedItems] = useState<number[]>([])
+    const [selectedFile, setSelectedFile] = useState<File>()
 
     const [initialPosition, setInitialPosition] = useState<[number, number]>([0, 0])
 
@@ -117,15 +120,18 @@ const CreatePoint = () => {
         const [latitude, longitude] = selectedPosition
         const items = selectedItems
 
-        const data = {
-            name, 
-            email, 
-            whatsapp,
-            uf,
-            city,
-            latitude,
-            longitude,
-            items
+        const data = new FormData()
+        data.append('name', name)
+        data.append('email', email)
+        data.append('whatsapp', whatsapp)
+        data.append('uf', uf)
+        data.append('city', city)
+        data.append('latitude', String(latitude))
+        data.append('longitude', String(longitude))
+        data.append('items', items.join(','))
+        
+        if (selectedFile) {
+            data.append('image', selectedFile)
         }
 
         await api.post('points', data)
@@ -147,6 +153,8 @@ const CreatePoint = () => {
 
             <form onSubmit={handleSubmit}>
                 <h1>Cadastro do <br/> ponto de coleta</h1>
+
+                <Dropzone onFileUploaded={setSelectedFile} />
 
                 <fieldset>
                     <legend>
