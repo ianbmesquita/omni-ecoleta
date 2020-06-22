121c2f096830fef85e2af98883563e4862c2f939
Ian Bezerra de Mesquita <ian.mesquita@inep.gov.br>
2020-06-22 08:37:08 -0300

    Criação da funcionalidade de upload de arquivos

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,13 @@
 import express from 'express'
+import multer from 'multer'
+import multerConfig from './configurations/multer'
 
 import ItemsController from './controllers/ItemsController'
 import PointsController from './controllers/PointsController'
 
 const routes = express.Router()
+const upload = multer(multerConfig)
+
 const itemsController = new ItemsController()
 const pointsController = new PointsController()
 
@@ -13,7 +17,7 @@ routes.get('/', (request, response) => {
 
 routes.get('/items', itemsController.index)
 
-routes.post('/points', pointsController.create)
+routes.post('/points', upload.single('image'), pointsController.create)
 routes.get('/points', pointsController.index)
 routes.get('/points/:id', pointsController.show) 
 
--- server/src/server.ts
+++ server/src/server.ts
@@ -10,5 +10,6 @@ app.use(express.json())
 app.use(routes)
 
 app.use('/image', express.static(path.resolve(__dirname, '..', 'assets', 'images')))
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'assets', 'uploads')))
 
 app.listen(3333)
